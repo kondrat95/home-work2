893a469c779eb2e9a3b8037c86312618d00c5356
kondrat95 <vmkondratenko70@gmail.com>
2026-01-17 00:25:24 +0100

    Resolved merge conflicts

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import s from './Message.module.css'
-<<<<<<< HEAD
 import {message0} from "../HW1";
-=======
->>>>>>> 039147c80769dc492ed26fd72cdef6b3ae7eb448
 
 // нужно создать правильный тип вместо any
 export type MessagePropsType = any
@@ -15,7 +12,6 @@ const Message = (props: MessagePropsType) => {
             <div className={s.imageAndText}>
                 <img
                     id={'hw1-avatar-' + props.message.id}
-<<<<<<< HEAD
                     src={props.message.user.avatar}
                 />
                 <div className={s.text}>
@@ -24,33 +20,11 @@ const Message = (props: MessagePropsType) => {
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {props.message.message.text}
-=======
-                    // создаёт студент
-
-                    //
-                />
-                <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
->>>>>>> 039147c80769dc492ed26fd72cdef6b3ae7eb448
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
-<<<<<<< HEAD
                 {props.message.message.time}
-=======
-                {/*создаёт студент*/}
-
-                {/**/}
->>>>>>> 039147c80769dc492ed26fd72cdef6b3ae7eb448
             </div>
         </div>
     )
